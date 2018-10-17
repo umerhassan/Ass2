22468d673342c571e81ad776c383afa9dcb4e6fc
umerhassan <33638819+umerhassan@users.noreply.github.com>
2018-10-16 22:02:28 -0600

    newClientServer

--- client.c
+++ client.c
@@ -1,134 +1,83 @@
+/*
+ * ClientSocekt.c
+ *
+ *  Created on: Sep 13, 2018
+ *      Author: reza
+ */
 
-
-/* Include files for C socket programming and stuff */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
 #include <string.h>
 
+#include <ctype.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <strings.h>
+#include <netdb.h>
 
+int main() {
 
-/* Manifest constants used by client program */
-#define MAX_HOSTNAME_LENGTH 64
-#define MAX_WORD_LENGTH 100
-#define BYNAME 1
-#define MYPORTNUM 12346   /* must match the server's port! */
+	/* Address initialization */
+	struct sockaddr_in server;
+	int MYPORTNUM = 12345;
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(MYPORTNUM);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-/* Menu selections */
-#define ALLDONE 0
-#define ENTER 1
+	/* Create the listening socket */
+	int sock;
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		printf("Error in socket() while creating lstn_sock\n");
+		exit(-1);
+	}
 
-/* Prompt the user to enter a word */
-void printmenu()
-  {
-    printf("\n");
-    printf("Please choose from the following selections:\n");
-    printf("  1 - Enter a word\n");
-    printf("  0 - Exit program\n");
-    printf("Your desired menu selection? ");
-  }
+	/* Connect to TCP server */
+	int status;
+	status = connect(sock, (struct sockaddr *) &server,
+			sizeof(struct sockaddr_in));
+	if (status < 0) {
+		printf("Error in connect()\n");
+		exit(-1);
+	} else {
+		printf("Connected.\n");
+	}
 
-/* Main program of client */
-int main()
-  {
-    int sockfd, sockfd2;
-    char c;
-    struct sockaddr_in server;
-    struct hostent *hp;
-    char hostname[MAX_HOSTNAME_LENGTH];
-    char message[MAX_WORD_LENGTH];
-    char messageback[MAX_WORD_LENGTH];
-    int choice, len, bytes;
+	/* Send data*/
+	int count;
+	char message[1024] = { "Hi." };
+	count = send(sock, message, sizeof(message), 0);
+	if (count < 0) {
+		printf("Error in send()\n");
+	}
 
-    /* Initialization of server sockaddr data structure */
-    memset(&server, 0, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(MYPORTNUM);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
+	/* Receive data */
+	char rcv_message[1024];
+	count = recv(sock, rcv_message, sizeof(rcv_message), 0);
+	if (count < 0) {
+		printf("Error in recv()\n");
+	} else {
+		printf("Server: %s\n", rcv_message);
+	}
 
-#ifdef BYNAME
-    /* use a resolver to get the IP address for a domain name */
-    /* I did my testing using csx1 (136.159.5.25)    Carey */
-    strcpy(hostname, "csx1.cpsc.ucalgary.ca");
-    hp = gethostbyname(hostname);
-    if (hp == NULL)
-      {
-	fprintf(stderr, "%s: unknown host\n", hostname);
-	exit(1);
-      }
-    /* copy the IP address into the sockaddr structure */
-    bcopy(hp->h_addr, &server.sin_addr, hp->h_length);
-#else
-    /* hard code the IP address so you don't need hostname resolver */
-    server.sin_addr.s_addr = inet_addr("136.159.5.25");
-#endif
+	while (1) {
+		gets(message);
+		send(sock, message, sizeof(message), 0);
 
-    /* create the client socket for its transport-level end point */
-    if( (sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1 )
-      {
-	fprintf(stderr, "wordlengthclient: socket() call failed!\n");
-	exit(1);
-      }
+		if (strstr(message, "Bye") != NULL) {
+			exit(0);
+		}
+	}
 
-    /* connect the socket to the server's address using TCP */
-    if( connect(sockfd, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) == -1 )
-      {
-	fprintf(stderr, "wordlengthclient: connect() call failed!\n");
-	perror(NULL);
-	exit(1);
-      }
-
-    /* Print welcome banner */
-    printf("Welcome! I am the client end of the client-server demo!!\n\n");
-    printmenu();
-    scanf("%d", &choice);
-
-    /* main loop: read a word, send to server, and print answer received */
-    while( choice != ALLDONE )
-      {
-	if( choice == ENTER )
-	  {
-	    /* get rid of newline after the (integer) menu choice given */
-	    c = getchar();
-
-	    /* prompt user for the input */
-	    printf("Enter your word: ");
-	    len = 0;
-	    while( (c = getchar()) != '\n' )
-	      {
-		message[len] = c;
-		len++;
-	      }
-	    /* make sure the message is null-terminated in C */
-	    message[len] = '\0';
-
-	    /* send it to the server via the socket */
-	    send(sockfd, message, len, 0);
-
-	    /* see what the server sends back */
-	    if( (bytes = recv(sockfd, messageback, len, 0)) > 0 )
-	      {
-		/* make sure the message is null-terminated in C */
-		messageback[bytes] = '\0';
-		printf("Answer received from server: ");
-		printf("%s\n", messageback);
-	      }
-	    else
-	      {
-		/* an error condition if the server dies unexpectedly */
-		printf("Sorry, dude. Server failed!\n");
-		close(sockfd);
-		exit(1);
-	      }
-	  }
-	else printf("Invalid menu selection. Please try again.\n");
-
-	printmenu();
-	scanf("%d", &choice);
-      }
-
-    /* Program all done, so clean up and exit the client */
-    close(sockfd);
-    exit(0);
-  }
+	/* Close the socket */
+	close(sock);
+	return 0;
+}
